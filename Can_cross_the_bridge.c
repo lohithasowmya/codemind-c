3e67258959752620895ab20b6c19dff205d5ce10
lohithasowmya <119840023+lohithasowmya@users.noreply.github.com>
2022-12-06 10:18:14 +0530

    #include<stdio.h> void bridge(int x,int y,int z) {     int m;     m=(z-y)/x;     printf("%d",m); } int main() {     int x,y,z,m;     scanf("%d%d%d",&x,&y,&z);     bridge(x,y,z); }

--- Can_cross_the_bridge.c
+++ Can_cross_the_bridge.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
+void bridge(int x,int y,int z)
+{
+    int m;
+    m=(z-y)/x;
+    printf("%d",m);
+}
 int main()
 {
     int x,y,z,m;
     scanf("%d%d%d",&x,&y,&z);
-    m=(z-y)/x;
-    printf("%d",m);
+    bridge(x,y,z);
 }

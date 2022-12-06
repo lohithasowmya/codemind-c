567ba205b44b7cd5ed7437d75fd6b4efe82aaf46
lohithasowmya <119840023+lohithasowmya@users.noreply.github.com>
2022-12-06 10:06:19 +0530

    #include<stdio.h> void capacity(int s,int t,int b) {     int c;     c=s*t*b;     printf("%dkb",c); } int main() {     int s,t,b;     scanf("%d%d%d",&s,&t,&b);     capacity(s,t,b); }

--- Capacity.c
+++ Capacity.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int capacity(int s,int t,int b)
+void capacity(int s,int t,int b)
 {
     int c;
     c=s*t*b;
-    return c;
+    printf("%dkb",c);  
 }
 int main()
 {
     int s,t,b;
     scanf("%d%d%d",&s,&t,&b);
-    int x=capacity(s,t,b);
-    printf("%dkb",x);
+    capacity(s,t,b);
 }

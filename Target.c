fec0dd0c5143429e6a88c78c6621deba573c9240
lohithasowmya <119840023+lohithasowmya@users.noreply.github.com>
2022-12-06 11:15:13 +0530

    #include<stdio.h> int main() {     int p1P2,p2P2,p3P3,p4P4;     scanf("%d %d %d %d",&p1P2,&p2P2,&p3P3,&p4P4);     if(p1P2>=10 && p2P2>=10 &&p3P3>=10 &&p4P4>=10)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Target.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int p1P2,p2P2,p3P3,p4P4;
+    scanf("%d %d %d %d",&p1P2,&p2P2,&p3P3,&p4P4);
+    if(p1P2>=10 && p2P2>=10 &&p3P3>=10 &&p4P4>=10)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}

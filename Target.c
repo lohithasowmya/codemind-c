8920da58da05c024f1cec7578d8204c65a221100
lohithasowmya <119840023+lohithasowmya@users.noreply.github.com>
2022-12-06 11:26:37 +0530

    #include<stdio.h> void target(int p1P2,int p2P2,int p3P3,int p4P4) {     if(p1P2>=10 && p2P2>=10 &&p3P3>=10 &&p4P4>=10)     {         printf("YES");     }     else     {         printf("NO");     } } int main() {     int p1P2,p2P2,p3P3,p4P4;     scanf("%d %d %d %d",&p1P2,&p2P2,&p3P3,&p4P4);     target(p1P2,p2P2,p3P3,p4P4); }

--- Target.c
+++ Target.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-int main()
+void target(int p1P2,int p2P2,int p3P3,int p4P4)
 {
-    int p1P2,p2P2,p3P3,p4P4;
-    scanf("%d %d %d %d",&p1P2,&p2P2,&p3P3,&p4P4);
     if(p1P2>=10 && p2P2>=10 &&p3P3>=10 &&p4P4>=10)
     {
         printf("YES");
@@ -11,4 +9,10 @@ int main()
     {
         printf("NO");
     }
+}
+int main()
+{
+    int p1P2,p2P2,p3P3,p4P4;
+    scanf("%d %d %d %d",&p1P2,&p2P2,&p3P3,&p4P4);
+    target(p1P2,p2P2,p3P3,p4P4);
 }

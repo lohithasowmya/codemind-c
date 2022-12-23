ef1547f9ff3d487e22a4d181bdacfdd5b061bbe1
lohithasowmya <119840023+lohithasowmya@users.noreply.github.com>
2022-12-23 19:44:53 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d %d",&x,&y);     float d;     d=x/y;     if(d>=30)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Expenditure.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    float d;
+    d=x/y;
+    if(d>=30)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
